b61584445e6ece55e7dfd230a6cbe7c056789018
Eduardo Augusto Coleta de Souza <souzaeduardoac@debian.XU555B>
2020-04-16 22:04:48 -0300

    #Added some spacing

--- try_flutter/lib/pages/home_page.dart
+++ try_flutter/lib/pages/home_page.dart
@@ -11,27 +11,62 @@ class HomePage extends StatelessWidget {
   }
 
   _body() => Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _text(),
+          _carousel(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _button('ListView'),
+              _button('Page 2'),
+              _button('Page 3'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _button('Snack'),
+              _button('Dialog'),
+              _button('Toast'),
+            ],
+          ),
+        ],
+      ));
+
+  _carousel() => Container(
+        margin: EdgeInsets.all(20),
+        height: 300,
+        child: PageView(
           children: <Widget>[
-            _text(),
-            _img(),
-            _button()
+            _img(
+                'https://huskyadvisor.com/wp-content/uploads/2019/08/husky-puppy-150x150.jpg'),
+            _img(
+                'https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2013/04/husky_qa.jpg.jpeg'),
+            _img(
+                'https://petshopdamadre.com.br/wp-content/uploads/2018/12/husky-e1544223858680-510x510.jpg'),
+            _img(
+                'https://www.thesprucepets.com/thmb/T39xIBoItE7jzRSEBJRInGg9pCA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/breed_profile_husky_1118000_hero_690-0158e427a4734e0fae59e2e91a7fe6e4.jpg'),
+            _img(
+                'https://cdn11.bigcommerce.com/s-oe2q4reh/images/stencil/160w/products/738/1297/Silver_Siberian_Husky_Puppy__26120.1568507116.jpg?c=2 '),
           ],
-        )
+        ),
       );
 
   _wholeScreenImage() => SizedBox.expand(
-        child: _img(),
+        child: _img(
+            'https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2013/04/husky_qa.jpg.jpeg'),
       );
 
   _centeredImage() => Center(
-        child: _img(),
+        child: _img(
+            'https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2013/04/husky_qa.jpg.jpeg'),
       );
 
-  _img() => Image.network(
-        'https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2013/04/husky_qa.jpg.jpeg',
+  _img(String imgPath) => Image.network(
+        imgPath,
         fit: BoxFit.cover,
       );
 
@@ -47,10 +82,10 @@ class HomePage extends StatelessWidget {
             decorationStyle: TextDecorationStyle.wavy),
       );
 
-  _button() => RaisedButton(
+  _button(String text) => RaisedButton(
         color: Colors.blueGrey,
         child: Text(
-          'OK',
+          text,
           style: TextStyle(color: Colors.white),
         ),
         onPressed: _onClickOk,
