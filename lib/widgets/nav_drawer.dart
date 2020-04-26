e7acc334e50124b3d491dbce32e4813157692cbb
Eduardo Augusto Coleta de Souza <souzaeduardoac@debian.XU555B>
2020-04-25 21:09:15 -0300

    NavDrawer functions

--- lib/widgets/nav_drawer.dart
+++ lib/widgets/nav_drawer.dart
@@ -3,39 +3,48 @@ import 'package:flutter/material.dart';
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text('Favoritos'),
-            subtitle: Text('mais informações...'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print('1');
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text('Ajuda'),
-            subtitle: Text('mais informações...'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print('1');
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print('1');
-              Navigator.pop(context);
-            },
-          ),
-        ],
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Eduardo Souza'),
+              accountEmail: Text('souzaeduardoac@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('https://cdn11.bigcommerce.com/s-oe2q4reh/images/stencil/160w/products/738/1297/Silver_Siberian_Husky_Puppy__26120.1568507116.jpg?c=2 '),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Favoritos'),
+              subtitle: Text('mais informações...'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('1');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text('Ajuda'),
+              subtitle: Text('mais informações...'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('1');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Logout'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('1');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
